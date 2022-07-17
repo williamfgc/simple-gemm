86b2d1111d2a94c40662a3d2ad2c03826e0d6ea4
William F Godoy <williamfgc@yahoo.com>
2022-07-16 21:26:39 -0400

    dgemm alpha and beta types

--- c/gemm-dense-cublas64.cu
+++ c/gemm-dense-cublas64.cu
@@ -150,8 +150,11 @@ int main(int argc, char *argv[]) {
   // Create a handle for CUBLAS
   cublasHandle_t handle;
   cublasCreate(&handle);
+  const double alpha = 1.;
+  const double beta = 0.;
+
   cublasDgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, A_rows, A_cols, B_cols, 
-     1.0, A_d, A_rows, B_d, B_cols, 0., C_d, A_rows);
+     &alpha, A_d, A_rows, B_d, B_cols, &beta, C_d, A_rows);
   cudaDeviceSynchronize();
   tmp = print_dtime(tmp, "cublas gemm");
 
@@ -163,7 +166,7 @@ int main(int argc, char *argv[]) {
     for (i = 1; i < steps; ++i) {
       clock_gettime(CLOCK_MONOTONIC_RAW, &start_i);
       cublasDgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, A_rows, A_cols, B_cols, 
-                  1.0, A_d, A_rows, B_d, B_cols, 0., C_d, A_rows);
+                  &alpha, A_d, A_rows, B_d, B_cols, &beta, C_d, A_rows);
       cudaDeviceSynchronize();
       end_i = print_dtime(start_i, "cublas gemm");
       average_time += dtime(start_i, end_i);
